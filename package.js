1c97fe43901526eb7b344c15a549cd4a10e3057d
Marco Pfeiffer <m4pfeiffer+git@gmail.com>
2014-08-23 23:46:37 +0200

    update for meteor 0.9

--- package.js
+++ package.js
@@ -1,13 +1,16 @@
 Package.describe({
-  summary: "helper to create reusable and extendable collection queries/selectors"
+  summary: "helper to create reusable and extendable collection queries/selectors",
+  version: "1.1.0",
+  git: "https://github.com/Nemo64/meteor-query-builder.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR-CORE@0.9.0-atm");
   api.use(['underscore', 'mongo-livedata']);
   api.add_files(['query-builder.js']);
 });
 
 Package.on_test(function (api) {
-  api.use(['query-builder', 'tinytest']);
+  api.use(["nemo64:query-builder", 'tinytest']);
   api.add_files(['query-builder-test.js']);
 });
